bfa74594165dcc657ca60ce9f38a37270144e6f9
Alejandro López Portillo Zabre <alpz13_2@hotmail.com>
2018-05-30 21:55:04 -0500

    Chapter03
    Working with pug views

--- chapter03/routes/index.js
+++ chapter03/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const bodyParser = require('body-parser');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' , text: 'Hello World'});
 });
+
+/* GET home page. */
+router.get('/s', function(req, res, next) {
+  res.render('index', { title: 'Express' , text: 'Hello World', user:{username: 'Mario'}});
+});
+
+
+router.post('/login', function(req, res, next) {
+	res.render('index', {title: 'Login', user: {username: req.body.username}})
+});
+
 
 module.exports = router;
